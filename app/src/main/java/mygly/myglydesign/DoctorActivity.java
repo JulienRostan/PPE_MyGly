23431ab03ddde50f3787ac86d1395cdbb8d329b1
JulienRostan <rostan.julien@gmail.com>
2015-04-03 15:58:31 +0200

    Design

---
+++ app/src/main/java/mygly/myglydesign/DoctorActivity.java
@@ -0,0 +1,31 @@
+package mygly.myglydesign;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by julien on 01/04/2015.
+ */
+public class DoctorActivity extends ActionBarActivity {
+
+    ListView lvListe;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        lvListe = (ListView)findViewById(R.id.lvListe);
+
+        String[] listeStrings = {"France","Allemagne","Russie"};
+
+        lvListe.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,listeStrings));
+
+    }
+
